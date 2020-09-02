a6bdd69b2bcc34e3f36932a0d68ccc76dc8b85da
Caiango <cvff12345@gmail.com>
2020-09-02 16:04:01 -0300

    Funcionalidades passo 1 e 2 funcionando

--- app/src/main/java/com/example/meal_plan/LayChanges.kt
+++ app/src/main/java/com/example/meal_plan/LayChanges.kt
@@ -1,74 +1,141 @@
 package com.example.meal_plan
 
 import android.app.Activity
+import android.view.View
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isInvisible
+import com.example.meal_plan.activities.MenuActivity
 import kotlinx.android.synthetic.main.activity_radio.*
+import kotlinx.android.synthetic.main.row_mhs.view.*
 
+
+lateinit var act: Activity
 
 class LayChanges {
-
-
+    var limitCount = 0
     fun changePass(activity: Activity) {
+        act = activity
         when {
             activity.radioButtonCopo.isChecked -> {
 
-                activity.radioopt1.text = "Copinho: 1,80ml"
-                activity.radioopt2.isInvisible = true
-                activity.radioopt3.isInvisible = true
-                activity.radioopt1.isInvisible = false
-                activity.txValor1.text = "R$ 3,00"
-                activity.txValor1.isInvisible = false
-                activity.txValor2.isInvisible = true
-                activity.txValor3.isInvisible = true
-                activity.txPasso2Desc.text = "2 Escolhas | ${activity.radioopt1.text.toString()}"
+                setupView(act.radioopt1, "Copinho: 1,80ml", false)
+                setupView(act.radioopt2, "", true)
+                setupView(act.radioopt3, "", true)
+                setupView(act.txValor1, "R$ 3,00", false)
+                setupView(act.txValor2, "", true)
+                setupView(act.txValor3, "", true)
+                setupView(act.txPasso2Desc, "Apenas Adicional / Passo 7", false)
+                activateCheck(act.cb21, false)
+                activateCheck(act.cb22, false)
+                activateCheck(act.cb23, false)
+                activateCheck(act.cb24, false)
+                activateCheck(act.cb25, false)
+                activateCheck(act.cb26, false)
+                getValueTam()
+
+
 
             }
             activity.radioButtonIso.isChecked -> {
-                activity.radioopt1.text = "H1"
-                activity.radioopt2.text = "H2"
-                activity.radioopt3.text = "H3"
-                activity.radioopt1.isInvisible = false
-                activity.radioopt2.isInvisible = false
-                activity.radioopt3.isInvisible = false
-                activity.txValor1.text = "R$ 5,00"
-                activity.txValor2.text = "R$ 8,00"
-                activity.txValor3.text = "R$ 10,00"
-                activity.txValor1.isInvisible = false
-                activity.txValor2.isInvisible = false
-                activity.txValor3.isInvisible = false
+
+                setupView(act.radioopt1, "H1", false)
+                setupView(act.radioopt2, "H2", false)
+                setupView(act.radioopt3, "H3", false)
+                setupView(act.txValor1, "R$ 5,00", false)
+                setupView(act.txValor2, "R$ 8,00", false)
+                setupView(act.txValor3, "R$ 10,00", false)
+                setupView(act.txPasso2Desc, "1 Escolha", false)
+                activateCheck(act.cb21, true)
+                activateCheck(act.cb22, true)
+                activateCheck(act.cb23, true)
+                activateCheck(act.cb24, true)
+                activateCheck(act.cb25, true)
+                activateCheck(act.cb26, true)
+                limitCount = 1
+                getValueTam()
+
             }
             activity.radioButtonTij.isChecked -> {
-                activity.radioopt1.text = "Pequeno"
-                activity.radioopt2.text = "Médio"
-                activity.radioopt3.text = "Grande"
-                activity.radioopt1.isInvisible = false
-                activity.radioopt2.isInvisible = false
-                activity.radioopt3.isInvisible = false
-                activity.txValor1.text = "R$ 4,00"
-                activity.txValor2.text = "R$ 7,00"
-                activity.txValor3.text = "R$ 10,00"
-                activity.txValor1.isInvisible = false
-                activity.txValor2.isInvisible = false
-                activity.txValor3.isInvisible = false
+
+                setupView(act.radioopt1, "Pequeno", false)
+                setupView(act.radioopt2, "Médio", false)
+                setupView(act.radioopt3, "Grande", false)
+                setupView(act.txValor1, "R$ 4,00", false)
+                setupView(act.txValor2, "R$ 7,00", false)
+                setupView(act.txValor3, "R$ 10,00", false)
+                setupView(act.txPasso2Desc, "1 Escolha", false)
+                activateCheck(act.cb21, true)
+                activateCheck(act.cb22, true)
+                activateCheck(act.cb23, true)
+                activateCheck(act.cb24, true)
+                activateCheck(act.cb25, true)
+                activateCheck(act.cb26, true)
+                limitCount = 1
+                getValueTam()
+
             }
             activity.radioButtonTac.isChecked -> {
-                activity.radioopt1.text = "Pequeno"
-                activity.radioopt2.text = "Médio"
-                activity.radioopt3.text = "Grande"
-                activity.radioopt1.isInvisible = false
-                activity.radioopt2.isInvisible = false
-                activity.radioopt3.isInvisible = false
-                activity.txValor1.text = "R$ 5,00"
-                activity.txValor2.text = "R$ 10,00"
-                activity.txValor3.text = "R$ 15,00"
-                activity.txValor1.isInvisible = false
-                activity.txValor2.isInvisible = false
-                activity.txValor3.isInvisible = false
+
+                setupView(act.radioopt1, "Pequeno", false)
+                setupView(act.radioopt2, "Médio", false)
+                setupView(act.radioopt3, "Grande", false)
+                setupView(act.txValor1, "R$ 5,00", false)
+                setupView(act.txValor2, "R$ 10,00", false)
+                setupView(act.txValor3, "R$ 15,00", false)
+                setupView(act.txPasso2Desc, "1 Escolha", false)
+                activateCheck(act.cb21, true)
+                activateCheck(act.cb22, true)
+                activateCheck(act.cb23, true)
+                activateCheck(act.cb24, true)
+                activateCheck(act.cb25, true)
+                activateCheck(act.cb26, true)
+                limitCount = 1
+                getValueTam()
+
             }
         }
     }
 
+    fun setupView(v: View, text: String, isInvi: Boolean) {
+        if (v is TextView) {
+            v.text = text.trim()
+            v.isInvisible = isInvi
+        } else if (v is RadioButton) {
+            v.text = text.trim()
+            v.isInvisible = isInvi
+        }
+
+    }
+
+    fun getValueTam(){
+        when{
+            act.radioopt1.isChecked -> {
+                var tx = act.txValor1.text.toString()
+                Toast.makeText(act.applicationContext, tx, Toast.LENGTH_LONG).show()
+            }
+            act.radioopt2.isChecked -> {
+                var tx = act.txValor2.text.toString()
+                Toast.makeText(act.applicationContext, tx, Toast.LENGTH_LONG).show()
+            }
+            act.radioopt3.isChecked ->{
+                var tx = act.txValor3.text.toString()
+                Toast.makeText(act.applicationContext, tx, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun activateCheck(v: View, isSel: Boolean){
+        if(v is CheckBox){
+            v.isEnabled = isSel
+        }
+    }
 }
+
+
+
 
 
 
--- app/src/main/java/com/example/meal_plan/activities/MenuActivity.kt
+++ app/src/main/java/com/example/meal_plan/activities/MenuActivity.kt
@@ -1,22 +1,50 @@
 package com.example.meal_plan.activities
 
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isInvisible
 import com.example.meal_plan.LayChanges
+import com.example.meal_plan.MainActivity
 import com.example.meal_plan.R
 import kotlinx.android.synthetic.main.activity_radio.*
+import java.lang.Exception
 
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
 
     var mLay = LayChanges()
+
+    var creme: ArrayList<CheckBox> = ArrayList()
+    var selCreme: ArrayList<String> = ArrayList()
+
+    lateinit var rbGroup2: RadioButton
+
+
+    var count = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_radio)
 
+
+        creme.add(cb21)
+        creme.add(cb22)
+        creme.add(cb23)
+        creme.add(cb24)
+        creme.add(cb25)
+        creme.add(cb26)
+
+        cb21.setOnCheckedChangeListener(this)
+        cb22.setOnCheckedChangeListener(this)
+        cb23.setOnCheckedChangeListener(this)
+        cb24.setOnCheckedChangeListener(this)
+        cb25.setOnCheckedChangeListener(this)
+        cb26.setOnCheckedChangeListener(this)
 
 
 
@@ -32,6 +60,58 @@ class MenuActivity : AppCompatActivity() {
         txValor3.isInvisible = true
 
     }
+
+    override fun onCheckedChanged(cb: CompoundButton?, isChecked: Boolean) {
+
+        if (isChecked) {
+            count++
+            if (count == mLay.limitCount) {
+                for (check: CheckBox in creme) {
+                    check.isEnabled = check.isChecked
+                    if (check.isChecked) {
+                        selCreme.add(check.text.toString())
+
+                    }
+                }
+                Toast.makeText(applicationContext, selCreme.toString(), Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            count--
+            if (count < 1) {
+                for (check: CheckBox in creme) {
+                    check.isEnabled = true
+                    selCreme.clear()
+                }
+            }
+        }
+
+
+    }
+
+//    fun getSelCheckBox() {
+//        for (check: CheckBox in creme) {
+//            if (check.isChecked) {
+//                selCreme.clear()
+//                selCreme.add(check.text.toString())
+//                Toast.makeText(applicationContext, selCreme.first(), Toast.LENGTH_LONG).show()
+//            }
+//        }
+//    }
+
+
+    //funçao para obter os valores do elemento do radiogroup2 marcado
+    fun onChangedRadio() {
+        try {
+            var selectID = radioGroup2.checkedRadioButtonId
+            rbGroup2 = findViewById(selectID)
+            Toast.makeText(applicationContext, rbGroup2.text, Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Toast.makeText(applicationContext, "Não foi selecionado um tamanho", Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
 
 
 }
